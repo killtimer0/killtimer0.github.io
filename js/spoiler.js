66f09c2f00282397c92d33a4eb1aead245f6185f
a2002 <1837009039@qq.com>
2021-09-20 00:35:04 +0800

    Site updated: 2021-09-20 00:35:03

---
+++ js/spoiler.js
@@ -0,0 +1,1 @@
+"use strict";!function(e){[].forEach.call(e.getElementsByClassName("spoiler"),function(e){e.getElementsByClassName("spoiler-title")[0].onclick=function(){e.classList.toggle("collapsed"),e.classList.toggle("expanded")}})}(document);
