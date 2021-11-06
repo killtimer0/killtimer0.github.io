64ea2a6ccf527f09d085ba961a1b636446a0d050
a2002 <1837009039@qq.com>
2021-11-06 17:23:25 +0800

    Site updated: 2021-11-06 17:23:21

--- js/game/justforfun.js
+++ js/game/justforfun.js
@@ -1,84 +1,76 @@
-/* 没错这个文件就是彩蛋hhh */
+/* 真·彩蛋 */
 
-var fontSize = 20, delta = 20, period = 800, border = 2;
-var pp1 = 8, pp2 = 6, rate = 0.2;
+var delta = 20, period = 1000, border = 10;
+var pp1 = 8, pp2 = 6;
 
-function initEscapeObj(name, tl){
-	var canvas = document.getElementById(name);
-	if (!canvas) return;
-	var ctx = canvas.getContext("2d");
-	var width = canvas.width, height = canvas.height;
-	ctx.font = fontSize + "px Arial";
-	var x = 0, y = 0, ox = 0, oy = 0;
-	var tWidth, tHeight, bWidth, bHeight, cWidth, cHeight;
-	var inEasing = false, process = 0, timer = 0, idx = 0;
-	var text; setText(tl[idx++]);
-	function calcValidRange(total, border, rate, len){
-		return (total - 2 * border - 2 * rate * len) / (1 + 2 * rate);
-	}
+function px(val){return val + 'px';}
+function initEscapeObj(name, href, width, height, tl){
+	var div = document.getElementById(name);
+	if (!div) return;
+	div.style.width = px(width), div.style.height = px(height);
+	div.style.padding = div.style.margin = '0';
+	var x = 0, y = 0, ox = 0, oy = 0, nx = 0, ny = 0;
+	var tWidth, tHeight, bWidth, bHeight;
+	var inEasing = false, process = 0, idx = 0;
+	var obj, raf; setText(tl[idx++]);
 	function setText(t){
-		text = t;
-		var tm = ctx.measureText(text);
-		tWidth = tm.width, tHeight = fontSize;
-		cWidth = calcValidRange(width, border, rate, tWidth);
-		bWidth = (width - cWidth) / 2;
-		cHeight = calcValidRange(height, border, rate, tHeight);
-		bHeight = (height - cHeight) / 2;
+		if (!obj){
+			obj = document.createElement('a');
+			obj.href = href;
+			obj.style.position = 'relative';
+			obj.style.userSelect = obj.style.transition = 'none';
+			obj.addEventListener("click", onMove);
+			obj.addEventListener("mousemove", onMove);
+			div.appendChild(obj);
+		}
+		obj.innerText = t;
+		tWidth = obj.offsetWidth, tHeight = obj.offsetHeight;
+		bWidth = bHeight = border;
 	}
-	function _easing_proc(nx, ny){
-		// 阻尼振动, min = -rate
+	function _easing_proc(){
+		// 阻尼振动, min = -rate = -0.288
+		raf = requestAnimationFrame(_easing_proc);
+		if (process >= period){
+			obj.style.left = px(nx), obj.style.top = px(ny);
+			cancelAnimationFrame(raf);
+			x = nx, y = ny;
+			inEasing = false;
+			return;
+		}
 		var p = process / period;
 		var factor = Math.exp(-pp1 * p) * Math.cos(p * pp2 * Math.PI);
 		x = nx + (ox - nx) * factor, y = ny + (oy - ny) * factor;
-		ctx.clearRect(0, 0, width, height);
-		ctx.fillText(text, x, y);
+		obj.style.left = px(x), obj.style.top = px(y);
 		process += delta;
-		if (process >= period){
-			ctx.clearRect(0, 0, width, height);
-			ctx.fillText(text, nx, ny);
-			clearInterval(timer);
-			x = nx, y = ny;
-			inEasing = false;
-		}
 	}
-	function update(bEasing, nx, ny){
+	function update(bEasing, _nx, _ny){
 		if (bEasing){
+			nx = _nx, ny = _ny;
 			inEasing = true, process = 0;
-			timer = setInterval(_easing_proc, delta, nx, ny);
-		}else{
-			ctx.clearRect(0, 0, width, height);
-			ctx.fillText(text, nx, ny);
-		}
-	}
-	function init(){
-		canvas.addEventListener("click", onMove);
-		canvas.addEventListener("mousemove", onMove);
-		x = (width - tWidth) / 2, y = (height + tHeight) / 2;
-		update(false, x, y);
+			raf = requestAnimationFrame(_easing_proc);
+		}else obj.style.left = px(_nx), obj.style.top = px(_ny);
 	}
 	function hitTest(px, py, cx, cy){
 		if (cx === undefined) cx = x, cy = y;
 		return px >= cx && py < cy && px < cx + tWidth && py >= cy - tHeight;
 	}
 	function calcNewPos(px, py){
-		var nx, ny;
-		do{
-			nx = Math.random() * (width - 2 * bWidth - tWidth) + bWidth, ny = Math.random() * (height - 2 * bHeight - tHeight) + tHeight + bHeight;
+		var nx, ny; do{
+			nx = Math.random() * (width - 2 * bWidth - tWidth) + bWidth, ny = Math.random() * (height - 2 * bHeight - tHeight) + bHeight;
 		} while (hitTest(px, py, nx, ny));
 		ox = x, oy = y;
 		update(true, nx, ny);
 	}
 	function onMove(e){
-		var px = e.offsetX, py = e.offsetY;
-		if (hitTest(px, py) && !(inEasing && process * pp2 < period)){
-			if (inEasing){
-				inEasing = false;
-				clearInterval(timer);
-			}
-			this.style.cursor = 'hand';
+		var px = e.offsetX + x, py = e.offsetY + y;
+		if (inEasing){
+			if (process * pp2 >= period) inEasing = false, cancelAnimationFrame(raf);
+		}else{
 			if (idx < tl.length) setText(tl[idx++]);
 			calcNewPos(px, py);
-		}else this.style.cursor = '';
+		}
+		e.preventDefault();
 	}
-	init();
+	x = (width - tWidth) / 2, y = (height - tHeight) / 2;
+	update(false, x, y);
 }
